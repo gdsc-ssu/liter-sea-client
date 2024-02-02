3a7844a3736eb68a7241d75952ab66bd9d8b4847
SuJin <kimsujin001127@gmail.com>
2024-02-02 17:36:37 +0900

    feat: 기본 레이아웃 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
 
 function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div>main</div>} />
-        <Route path="/signin" element={<div>signin</div>} />
-        <Route path="/review" element={<div>review</div>} />
-        <Route path="/signout" element={<div>signout</div>} />
-        <Route path="/stage" element={<div>stage</div>} />
-        <Route path="/summary" element={<div>summary</div>} />
-        <Route path="/vocabulary" element={<div>vocabulary</div>} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<div>main</div>} />
+          <Route path="/signin" element={<div>signin</div>} />
+          <Route path="/review" element={<div>review</div>} />
+          <Route path="/signout" element={<div>signout</div>} />
+          <Route path="/stage" element={<div>stage</div>} />
+          <Route path="/summary" element={<div>summary</div>} />
+          <Route path="/vocabulary" element={<div>vocabulary</div>} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
