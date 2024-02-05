5562a931561a8a59b5439d76a54fbb0fb32a2da8
SuJin <kimsujin001127@gmail.com>
2024-02-06 00:02:00 +0900

    feat: 선택한 글자 뜻 팝업

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,15 +1,36 @@
+import { COLORS } from "@/styles/colors";
+import { useState } from "react";
 import styled from "styled-components";
+import VocaModal from "../VocaModal/VocaModal";
 
 const Text = () => {
+  const [clickedIdx, setIsClickedIdx] = useState(-1);
+  const data =
+    "대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴 내용";
+  const dataArr = data.split(" ");
+
   return (
     <Container>
       <TitleBox>제목</TitleBox>
       <TextBox>
-        대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴 내용대충
-        엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청
-        긴 내용대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴 내용대충 엄청 긴
-        내용대충 엄청 긴 내용
+        {dataArr.map((el, idx) => {
+          return (
+            <>
+              <Span
+                id="ClickedSpan"
+                clicked={idx === clickedIdx}
+                onClick={() => {
+                  setIsClickedIdx(idx);
+                }}
+              >
+                {el}
+              </Span>
+              <span> </span>
+            </>
+          );
+        })}
       </TextBox>
+      {clickedIdx > -1 && <VocaModal word={dataArr[clickedIdx]} />}
     </Container>
   );
 };
@@ -22,6 +43,10 @@ const TitleBox = styled.div``;
 
 const TextBox = styled.div`
   margin-top: 2rem;
+`;
+
+const Span = styled.span<{ clicked: boolean }>`
+  background-color: ${(props) => (props.clicked ? COLORS.blue : "transparent")};
 `;
 
 export default Text;
