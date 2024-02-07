5681528eb76735d58e21158f1390f281c3200ec4
Seongna0 <sna0e@naver.com>
2024-02-07 22:19:12 +0900

    Router 일치

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Header from "./components/Header/Header";
-import Vocabulary from "./pages/Vocabulary/Vocabulary";
+import Multiple from "./pages/Stage/StageCheck/Multiple";
+import Vocabulary from "./pages/Stage/StageCheck/Vocabulary";
+import ResultLoading from "./pages/ResultLoading/ResultLoading";
+import StageResult from "./pages/Stage/Result/StageResult";
+import TodaySummary from "./pages/TodaySummary/TodaySummary";
+import SummaryResult from "./pages/TodaySummary/SummaryResult";
+import ReviewNote from "./pages/ReviewNote/ReviewNote";
+import Summary from "./pages/ReviewNote/Summary";
 
 function Router() {
   return (
@@ -11,11 +18,24 @@ function Router() {
         <Routes>
           <Route path="/" element={<div>main</div>} />
           <Route path="/signin" element={<div>signin</div>} />
-          <Route path="/review" element={<div>review</div>} />
           <Route path="/signout" element={<div>signout</div>} />
-          <Route path="/stage" element={<div>stage</div>} />
-          <Route path="/summary" element={<div>summary</div>} />
-          <Route path="/vocabulary" element={<Vocabulary/>} />
+
+          {/** 단계별 확인 */}
+          <Route path="/stage/1" element={<Multiple />} />
+          <Route path="/stage/2" element={<Vocabulary />} />
+          <Route path="/stage/result" element={<StageResult />} />
+
+          {/** 오늘의 요약 */}
+          <Route path="/summary/1" element={<TodaySummary />} />
+          <Route path="/summary/result" element={<SummaryResult />} />
+
+          {/* 복습 노트 */}
+          <Route path="/review" element={<ReviewNote />} />
+          <Route path="/review/1" element={<Summary />} />
+
+          <Route path="/vocabulary" element={<div>vocabulary</div>} />
+
+          <Route path="/loading" element={<ResultLoading />} />
         </Routes>
       </Layout>
     </BrowserRouter>
