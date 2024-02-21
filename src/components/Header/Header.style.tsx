0c6153590e80487279a534b9cce28569084e9154
SuJin <kimsujin001127@gmail.com>
2024-02-21 21:23:06 +0900

    style: header 세로 폭 축소

--- src/components/Header/Header.style.tsx
+++ src/components/Header/Header.style.tsx
@@ -1,19 +1,19 @@
 import styled from "styled-components";
 
 export const LogoBox = styled.div`
-	padding: 1rem 3rem 1rem 1rem;
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  padding: 0.1rem 3rem 0.1rem 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const MenuComponentBox = styled.div`
-	padding: 1rem 3rem;
-	display: flex;
-	align-items: center;
+  padding: 1rem 3rem;
+  display: flex;
+  align-items: center;
 `;
 
 export const ProfileImgBox = styled.div`
-	display: flex;
-	align-items: center;
+  display: flex;
+  align-items: center;
 `;
