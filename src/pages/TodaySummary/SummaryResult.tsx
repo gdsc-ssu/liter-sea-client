a3fd15defe69c889269504a52c66d8396d602338
SuJin <kimsujin001127@gmail.com>
2024-02-21 19:31:47 +0900

    style: 점수 제목 변경

--- src/pages/TodaySummary/SummaryResult.tsx
+++ src/pages/TodaySummary/SummaryResult.tsx
@@ -3,8 +3,10 @@ import PercentState from "@/components/PercentState/PercentState";
 import BlueButton from "@/components/Button/BlueButton";
 import FlexContainer from "@/components/common/flex-container";
 import { COLORS } from "@/styles/colors";
+import { useState } from "react";
 
 const SummaryResult = () => {
+  const [presentIdx, setPresentIdx] = useState<number>(1);
   const handleOnClick = () => {};
   return (
     <FlexContainer direction="column" alignItems="stretch">
@@ -13,14 +15,24 @@ const SummaryResult = () => {
         <FlexItem
           style={{ minWidth: "20rem", background: ` ${COLORS.lightGray}` }}
         >
-          <PercentState number={1} title="10xp 획득하기" percent={50} />
-          <PercentState number={2} title="10xp 획득하기" percent={50} />
-          <PercentState number={3} title="10xp 획득하기" percent={50} />
-          <PercentState number={4} title="10xp 획득하기" percent={50} />
-          <PercentState number={5} title="10xp 획득하기" percent={50} />
+          <div onClick={() => setPresentIdx(1)}>
+            <PercentState number={1} title="1번 지문" percent={50} />
+          </div>
+          <div onClick={() => setPresentIdx(2)}>
+            <PercentState number={2} title="2번 지문" percent={50} />
+          </div>
+          <div onClick={() => setPresentIdx(3)}>
+            <PercentState number={3} title="3번 지문" percent={50} />
+          </div>
+          <div onClick={() => setPresentIdx(4)}>
+            <PercentState number={4} title="4번 지문" percent={50} />
+          </div>
+          <div onClick={() => setPresentIdx(5)}>
+            <PercentState number={5} title="5번 지문" percent={50} />
+          </div>
         </FlexItem>
         <FlexItem style={{ background: `${COLORS.boxBg}` }}>
-          <PercentState number={4} title="10xp 획득하기" percent={50} />
+          <PercentState number={4} title="1번 지문" percent={50} />
           <FlexContainer style={{ padding: "0.5rem 1rem" }}>
             귀하의 답변은 문단의 주제와 주요 내용을 정확하게 파악하고 있습니다.
             또한, 문단의 세부 내용인 "3년 동안의 지속"과 "한국의 승리"를
