6bdcb4ab219305e60f91441d297e45d41ec697d1
Seongna0 <sna0e@naver.com>
2024-02-21 21:35:39 +0900

    [#24] 단어장 페이지 API 연결

--- src/pages/Vocabulary/VocabularyContents.tsx
+++ src/pages/Vocabulary/VocabularyContents.tsx
@@ -8,62 +8,62 @@ import { COLORS } from "@/styles/colors";
 import FlexContainer from "@/components/common/flex-container";
 
 const VOCABOOK_CONTENTS = [
-	{ word: "word", meaning: "meaning", example: "example" },
-	{ word: "word2", meaning: "meaning2", example: "example2" },
-	{ word: "word3", meaning: "meaning3", example: "example3" },
+  { word: "word", meaning: "meaning", example: "example" },
+  { word: "word2", meaning: "meaning2", example: "example2" },
+  { word: "word3", meaning: "meaning3", example: "example3" },
 ];
 
 const VocaburalyContents = () => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const location = useLocation();
+  const location = useLocation();
 
-	function handleGoToBack() {
-		navigate(-1);
-	}
+  function handleGoToBack() {
+    navigate(-1);
+  }
 
-	function handleGotoStudyWords() {
-		navigate("/vocabulary/id/study", {
-			state: { title: location.state.title, contents: VOCABOOK_CONTENTS },
-		});
-	}
+  function handleGotoStudyWords() {
+    navigate("/vocabulary/id/study", {
+      state: { title: location.state.title, contents: VOCABOOK_CONTENTS },
+    });
+  }
 
-	return (
-		<FlexContainer direction="column" alignItems="stretch" gap={2}>
-			<BackButton onClick={handleGoToBack} />
-			<PageTitle>{location.state.title}</PageTitle>
-			<FlexContainer direction="column" alignItems="stretch" gap={2}>
-				{VOCABOOK_CONTENTS.map((item, index) => (
-					<WordContainer>
-						<WordContent
-							key={index}
-							word={item.word}
-							meaning={item.meaning}
-							example={item.example}
-						></WordContent>
-						<DeleteWordButtonContainer>
-							<DeleteWordButton />
-						</DeleteWordButtonContainer>
-					</WordContainer>
-				))}
-				<BlueButton
-					onClickFunc={handleGotoStudyWords}
-					text={"단어장 공부하기"}
-				/>
-			</FlexContainer>
-		</FlexContainer>
-	);
+  return (
+    <FlexContainer direction="column" alignItems="stretch" gap={2}>
+      <BackButton onClick={handleGoToBack} />
+      <PageTitle>{location.state?.title}</PageTitle>
+      <FlexContainer direction="column" alignItems="stretch" gap={2}>
+        {VOCABOOK_CONTENTS.map((item, index) => (
+          <WordContainer>
+            <WordContent
+              key={index}
+              word={item.word}
+              meaning={item.meaning}
+              example={item.example}
+            ></WordContent>
+            <DeleteWordButtonContainer>
+              <DeleteWordButton />
+            </DeleteWordButtonContainer>
+          </WordContainer>
+        ))}
+        <BlueButton
+          onClickFunc={handleGotoStudyWords}
+          text={"단어장 공부하기"}
+        />
+      </FlexContainer>
+    </FlexContainer>
+  );
 };
 
 export default VocaburalyContents;
 
 const PageTitle = styled.div`
-	color: ${COLORS.black};
+  color: ${COLORS.black};
 `;
 
 const WordContainer = styled(FlexContainer)`
-	display: flex;
-	align-items: center;
+  display: flex;
+  align-items: center;
 `;
 
 const DeleteWordButtonContainer = styled.div``;
